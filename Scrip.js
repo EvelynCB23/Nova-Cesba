32e7c799e473b2b06811afe79788e37984046bc9
Lizbeth-16 <2516192@cesba-queretaro.edu.mx>
2026-03-17 09:48:38 -0600

    prueba

---
+++ Scrip.js
@@ -0,0 +1,16 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // VARIABLES
+    let nombre = "Lizbeth";
+
+    // FUNCIONES
+    function saludar() {
+        console.log("Hola " + nombre);
+    }
+
+    // EJECUCIÓN
+    saludar();
+
+});
