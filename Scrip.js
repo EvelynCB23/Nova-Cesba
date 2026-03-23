e1d652e036f197da659624ff68e690f3f5f28152
Lizbeth-16 <2516192@cesba-queretaro.edu.mx>
2026-03-23 08:05:23 -0600

    primer comit

--- Scrip.js
+++ Scrip.js
@@ -1,16 +1,12 @@
-"use strict";
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
-document.addEventListener("DOMContentLoaded", () => {
+cargarEventListeners();
 
-    // VARIABLES
-    let nombre = "Lizbeth";
-
-    // FUNCIONES
-    function saludar() {
-        console.log("Hola " + nombre);
-    }
-
-    // EJECUCIÓN
-    saludar();
-
-});
+function cargarEventListeners(){
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
